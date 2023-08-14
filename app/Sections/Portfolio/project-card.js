5d00dd74d0e3a8848464c1c807dbefbe1c6331e4
Nick Knighton <nickk@tamu.edu>
2023-08-13 19:49:33 -0500

    Portfolio cards is adjusted, height is wrong, no technology list

--- app/Sections/Portfolio/project-card.js
+++ app/Sections/Portfolio/project-card.js
@@ -1,36 +1,35 @@
 import { Carousel } from "flowbite-react";
 import React from "react";
 import styles from "./carousel-button.css";
-import '../Portfolio/grid.css'
+import "../Portfolio/grid.css";
+import '../../../public/Project Photos/Personal Website/countries-visited.png'
 
+const ProjectCard = ({ project }) => {
+  return (
+    <div class="bg-white projectWidth rounded-lg">
+      <h1 class="text-gray-700 text-xl text-center font-bold m-6">{project.title}</h1>
 
-const ProjectCard = ({project}) => {
-    return (
-        <div class="bg-white projectWidth rounded-lg">
-            
-            <h2 class="text-gray-700 text-center font-bold m-6">
-                {project.title}
-            </h2>   
+      <div className="mx-auto h-3/4 px-4">
+        <Carousel slide={false} className="dark">
+          {project.images.map((image) => (
+            <img
+              src={image.src}
+              alt={`Image ${image.id}`}
+              className="rounded-lg object-contain h-64"
+            />
+          ))}
+        </Carousel>
+      </div>
 
-            <Carousel slide={false} className="dark mx-auto w-full">
-                {project.images.map((image) => (
-                <img
-                    src={image.src}
-                    alt={`Image ${image.id}`}
-                    className="rounded-lg w-3/4 object-contain h-1/2"
-                />
-                ))}
-            </Carousel>
-            
-            {/* <div class="flex flex-wrap justify-center items-center">
+      {/* <div class="flex flex-wrap justify-center items-center">
                 {project.technology.map((tech, index) => (
                     <span className="inline-block bg-gray-200 text-gray-700 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2 mt-2">
                 {tech}
                 </span>
             ))}
             </div> */}
-        </div>
-    )
-}
+    </div>
+  );
+};
 
 export default ProjectCard;
