34f1d94785a68bd3a0245988c0d7812fb3a69d1c
Nick Knighton <nickmknighton@gmail.com>
2023-08-13 20:00:04 -0500

    Added technology bubbles

--- app/Sections/Portfolio/project-card.js
+++ app/Sections/Portfolio/project-card.js
@@ -9,25 +9,25 @@ const ProjectCard = ({ project }) => {
     <div class="bg-white projectWidth rounded-lg">
       <h1 class="text-gray-700 text-xl text-center font-bold m-6">{project.title}</h1>
 
-      <div className="mx-auto h-3/4 px-4">
-        <Carousel slide={false} className="dark">
-          {project.images.map((image) => (
-            <img
-              src={image.src}
-              alt={`Image ${image.id}`}
-              className="rounded-lg object-contain h-64"
-            />
-          ))}
-        </Carousel>
-      </div>
+        <div className="mx-auto h-3/5 px-4">
+            <Carousel slide={false} className="dark">
+                {project.images.map((image) => (
+                    <img
+                    src={image.src}
+                    alt={`Image ${image.id}`}
+                    className="rounded-lg object-contain h-64"
+                    />
+                ))}
+            </Carousel>
+        </div>
 
-      {/* <div class="flex flex-wrap justify-center items-center">
-                {project.technology.map((tech, index) => (
-                    <span className="inline-block bg-gray-200 text-gray-700 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2 mt-2">
+        <div class="flex flex-wrap justify-center items-center mx-4">
+            {project.technology.map((tech, index) => (
+            <span className="inline-block bg-gray-200 text-gray-700 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2 mt-2">
                 {tech}
-                </span>
-            ))}
-            </div> */}
+            </span>
+        ))}
+        </div>
     </div>
   );
 };
